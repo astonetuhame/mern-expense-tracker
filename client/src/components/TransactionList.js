dc49f9f282e6a4d57ca77734e45d62d0d5a6fa28
Astone Tuhame <astonetuhame@gmail.com>
2021-12-15 22:07:48 +0300

    Update TransactionList.js

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,19 +1,23 @@
 import { GlobalContext } from "../context/GlobalState"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import Transaction from "./Transaction"
 
 
 const TransactionList = () => {
 
-    const { transactions } = useContext(GlobalContext)
+    const { transactions, getTransactions } = useContext(GlobalContext)
 
+    useEffect(() => {
+        getTransactions();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <>
             <h3>History</h3>
             <ul className="list">
                 {transactions.map((transaction, index) =>
-                ( <Transaction key={index} transaction={transaction}/> )
+                    (<Transaction key={index} transaction={transaction} />)
                 )}
             </ul>
         </>
