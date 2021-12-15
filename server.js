a774abcdac84e6d62e8d4230ff08912fc9aca784
Astone Tuhame <astonetuhame@gmail.com>
2021-12-15 18:58:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
-
+const connectDB = require('./config/db')
 
 dotenv.config({path: './config/config.env'});
 
+connectDB();
 
-const transactions = require('./routes/transactions')
+const transactions = require('./routes/transactions');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => res.send('Hello'));
 
-app.use('/api/v1/transactions', transactions)
+app.use('/api/v1/transactions', transactions);
 
 
 const PORT = process.env.PORT || 5000;
